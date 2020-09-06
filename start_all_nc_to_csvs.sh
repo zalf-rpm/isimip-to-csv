c2c0ceb3c7ffaba4f462eba2b2251bf7d6f01651
Michael Berg <michael.berg@zalf.de>
2020-09-06 14:48:07 +0200

    Update start_all_nc_to_csvs.sh

--- start_all_nc_to_csvs.sh
+++ start_all_nc_to_csvs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sh sbatch_transform_single_gcm_scen.sh GFDL-ESM4 historical /scratch/csvs /beegfs/common/data/climate/isimip/AgMIP.input_csvs/GFDL-ESM4/historical 70 87
+sbatch sbatch_transform_single_gcm_scen.sh GFDL-ESM4 historical /scratch/csvs /beegfs/common/data/climate/isimip/AgMIP.input_csvs/GFDL-ESM4/historical 70 87
